0a53662aa6c3a1a861ff64ec70a68920db723c6c
Vaibhav Sachdeva <vaibhavait04@gmail.com>
2015-04-30 08:03:33 +0530

    All progs with makefile from Sahni book - codes given online

---
+++ AllProgs/c1/sum.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std; 
+
+float sum (float *a, const int n)
+{
+ float s = 0;
+ for (int i = 0; i < n; i++) s += a[i];
+ return s;
+}
+
+float rsum (float *a, const int n)
+{
+ if (n <= 0) return 0;
+ else return (rsum(a, n-1) + a[n-1]);
+}
+
+int main()
+{
+ float b[10];
+ b[0] = 1.3;  b[1] = 2.43;  b[2] = 3.1; b[3] = 4;
+ cout << sum(b,4) << endl;
+ cout << rsum(b,4) << endl;
+}
---
+++ AllProgs/c6/multi.cpp
@@ -0,0 +1,34 @@
+#include <iostream> 
+using namespace std; 
+enum Boolean {FALSE, TRUE};
+
+class Graph;
+
+class GraphEdge {
+friend Graph;
+private:
+    Boolean m;
+    int vertex1, vertex2;
+    GraphEdge *path1, *path2;
+};
+
+typedef GraphEdge *EdgePtr;
+
+class Graph {
+private:
+    EdgePtr *HeadNodes;
+    int n;
+public:
+    Graph(const int);
+};
+
+Graph::Graph(const int vertices = 0) : n(vertices)
+{
+     HeadNodes = new EdgePtr[n];
+     for (int i = 0; i < n; i++) HeadNodes[i] = 0;
+}
+
+int main()
+{
+    Graph g(5);
+}
